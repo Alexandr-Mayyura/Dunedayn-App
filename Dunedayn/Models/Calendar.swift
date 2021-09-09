3a30835c68408d09d555af6133b3be9cd2896ee7
Alexandr-Mayyura <80215813+Alexandr-Mayyura@users.noreply.github.com>
2021-09-09 10:05:13 +0300

    add models

---
+++ Dunedayn/Models/Calendar.swift
@@ -0,0 +1,8 @@
+//
+//  Calendar.swift
+//  Dunedayn
+//
+//  Created by Aleksandr Mayyura on 09.09.2021.
+//
+
+import Foundation
---
+++ Dunedayn/Models/CalendarBase.swift
@@ -0,0 +1,8 @@
+//
+//  CalendarBase.swift
+//  Dunedayn
+//
+//  Created by Aleksandr Mayyura on 09.09.2021.
+//
+
+import Foundation
